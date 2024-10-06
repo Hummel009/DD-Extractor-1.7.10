a7a7edb23175c92fa74c382a8a9c4273dcdc9c32
Hummel009 <hummel.turbamentum@gmail.com>
2024-10-06 14:01:34 +0300

    ok

--- launch.bat
+++ launch.bat
@@ -1,4 +1,4 @@
-xcopy "%GRADLE_USER_HOME%\caches\minecraft\net\minecraftforge\forge\1.7.10-10.13.4.1614-1.7.10" "C:\Users\%USERNAME%\.gradle\caches\minecraft\net\minecraftforge\forge\1.7.10-10.13.4.1614-1.7.10" /E /I /Y
+xcopy "forge" "C:\Users\%USERNAME%\.gradle\caches\minecraft\net\minecraftforge\forge\1.7.10-10.13.4.1614-1.7.10" /E /I /Y
 
 java -jar data/deobfuscator.jar --inputJar mod/*.jar --outputJar "data/mod.jar" --mappingsVer "1.7.10-10.13.4.1614-1.7.10-shipped"
 
